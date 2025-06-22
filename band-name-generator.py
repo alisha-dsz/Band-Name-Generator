641e31166fa49bfaaf185818a3c72bd6459c061d
Alisha Dsouza <alishamarinadsouza@gmail.com>
2025-06-22 21:53:20 +0530

    Create band-name-generator.py

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+your_city_name = input("What's the name of the city you grow up in?\n")
+your_pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + your_city_name + " " + your_pet_name )
